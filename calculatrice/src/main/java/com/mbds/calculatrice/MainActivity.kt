6202cb73aa366893e8700171832d6a3bf1082d37
William POITEVIN <william.poitevin@gmail.com>
2020-10-22 23:18:50 +0200

    edit calculette

--- calculatrice/src/main/java/com/mbds/calculatrice/MainActivity.kt
+++ calculatrice/src/main/java/com/mbds/calculatrice/MainActivity.kt
@@ -17,35 +17,44 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initView()
+    }
 
+    private fun initView(){
         binding.root.children.filter {
             it is Button
         }.forEach {
-            it.setOnClickListener { button ->
-                when(button.tag?.toString()) {
-                    "NUMBER" -> binding.form.text = binding.form.text.toString() + (button as Button).text
-                    "AC" -> {
-                        binding.form.text = ""
-                        binding.res.text = ""
-                        nextOperation = Operation.NONE
-                    }
-                    "EQ" -> {
-                        val result = compute()
+            it.setOnClickListener { view ->
+                val button = view as Button
+                setListener(button)
+            }
+        }
+    }
 
-                        binding.form.text = result.toString()
-                        binding.res.text = ""
-                        nextOperation = Operation.NONE
-                    }
-                    "PLUS" -> addOperation(Operation.ADD)
-                    "MINUS" -> addOperation(Operation.MIN)
-                    "DIV" -> addOperation(Operation.DIV)
-                    "MULT" -> addOperation(Operation.MUL)
-                    else -> {
-                        binding.form.text = ""
-                        binding.res.text = ""
-                        nextOperation = Operation.NONE
-                    }
-                }
+
+    private fun setListener(button: Button){
+        when(button.tag?.toString()) {
+            "NUMBER" -> binding.form.text = binding.form.text.toString() + (button as Button).text
+            "AC" -> {
+                binding.form.text = ""
+                binding.res.text = ""
+                nextOperation = Operation.NONE
+            }
+            "EQ" -> {
+                val result = compute()
+
+                binding.form.text = result.toString()
+                binding.res.text = ""
+                nextOperation = Operation.NONE
+            }
+            "PLUS" -> addOperation(Operation.ADD)
+            "MINUS" -> addOperation(Operation.MIN)
+            "DIV" -> addOperation(Operation.DIV)
+            "MULT" -> addOperation(Operation.MUL)
+            else -> {
+                binding.form.text = ""
+                binding.res.text = ""
+                nextOperation = Operation.NONE
             }
         }
     }
