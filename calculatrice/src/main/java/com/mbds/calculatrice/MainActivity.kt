34fa83caa147644ad3127fcc36fb3826c7840a49
William POITEVIN <william.poitevin@gmail.com>
2020-10-22 23:15:27 +0200

    edit calculette

--- calculatrice/src/main/java/com/mbds/calculatrice/MainActivity.kt
+++ calculatrice/src/main/java/com/mbds/calculatrice/MainActivity.kt
@@ -1,39 +1,84 @@
 package com.mbds.calculatrice
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import com.mbds.calculatrice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
-
-
+    private lateinit var binding : ActivityMainBinding
+    private var nextOperation: Operation = Operation.NONE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(LayoutInflater.from(baseContext))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         binding.root.children.filter {
             it is Button
         }.forEach {
-            it.setOnClickListener { button -> binding.form.text = binding.form.text.toString() + (button as Button).text
-                /*when(button.tag?.toString()) {
+            it.setOnClickListener { button ->
+                when(button.tag?.toString()) {
                     "NUMBER" -> binding.form.text = binding.form.text.toString() + (button as Button).text
-                }*/
+                    "AC" -> {
+                        binding.form.text = ""
+                        binding.res.text = ""
+                        nextOperation = Operation.NONE
+                    }
+                    "EQ" -> {
+                        val result = compute()
 
+                        binding.form.text = result.toString()
+                        binding.res.text = ""
+                        nextOperation = Operation.NONE
+                    }
+                    "PLUS" -> addOperation(Operation.ADD)
+                    "MINUS" -> addOperation(Operation.MIN)
+                    "DIV" -> addOperation(Operation.DIV)
+                    "MULT" -> addOperation(Operation.MUL)
+                    else -> {
+                        binding.form.text = ""
+                        binding.res.text = ""
+                        nextOperation = Operation.NONE
+                    }
+                }
             }
         }
-
     }
 
+    private fun addOperation(operation: Operation){
+        val result = compute()
 
+        binding.form.text = ""
+        binding.res.text = result.toString()
+        nextOperation = operation
+    }
+
+    private fun compute() :Double{
+        var primaryOperator =  binding.res.text?.valueOrNull()?.toDouble() ?: 0.0
+        val secondaryOperator =  binding.form.text?.valueOrNull()?.toDouble() ?: 0.0
+        primaryOperator = when(nextOperation){
+            Operation.ADD -> primaryOperator + secondaryOperator
+            Operation.MIN -> primaryOperator - secondaryOperator
+            Operation.DIV -> primaryOperator / secondaryOperator
+            Operation.MUL -> primaryOperator * secondaryOperator
+            Operation.NONE -> secondaryOperator
+        }
+
+        return primaryOperator
+    }
+
+    enum class Operation(){
+        ADD,
+        MIN,
+        DIV,
+        MUL,
+        NONE
+    }
+
+    fun CharSequence.valueOrNull() : String? = if (isNullOrBlank()) null else toString()
 }
