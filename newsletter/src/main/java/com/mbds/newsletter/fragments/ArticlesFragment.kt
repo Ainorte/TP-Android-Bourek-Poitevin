bb2be4e0fc77306dff7089f70fba3686764db602
William POITEVIN <william.poitevin@gmail.com>
2020-10-23 19:29:25 +0200

    edit newsletter

--- newsletter/src/main/java/com/mbds/newsletter/fragments/ArticlesFragment.kt
+++ newsletter/src/main/java/com/mbds/newsletter/fragments/ArticlesFragment.kt
@@ -1,12 +1,12 @@
+
 package com.mbds.newsletter.fragments
 
-import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mbds.newsletter.R
